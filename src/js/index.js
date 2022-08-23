291f28dd408055c7d0e9fb57a8dee5d999a11a22
Andrey Brekotkin <brekot@ya.ru>
2022-08-23 11:56:30 +0300

    Block 14

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,17 @@
 import { Fancybox } from "@fancyapps/ui";
 
-import "./import/modules";
-import "./import/components";
+/* Проверка возможности использования webp */
+function canUseWebp()
+{
+    let elem = document.createElement('canvas');
+
+    if (!!(elem.getContext && elem.getContext('2d')))
+    {
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+
+    return false;
+}
+
+if (canUseWebp()) document.body.classList.add('can-use-webp');
+else document.body.classList.add('not-use-webp');
